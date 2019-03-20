f14b015a395f2235715223b2df653e5b12f8c1db
Wiktor <w.grudzina@gmail.com>
2019-03-20 18:56:22 +0100

    [dev] added template for animations
    created prototype animations (1 frame for each). Programmed animation behaviour for different input in enemy and player scripts.

--- Desktop/enemy/enemy.go
+++ Desktop/enemy/enemy.go
@@ -17,7 +17,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/main/main.atlas\"\n"
-  "default_animation: \"enemy_prot\"\n"
+  "default_animation: \"enemy_up\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
