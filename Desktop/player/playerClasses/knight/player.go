ae6325c32de19b0d5ec1034193906b049190b50c
Wiktor <w.grudzina@gmail.com>
2019-03-21 17:52:17 +0100

    [dev] Player class object factories
    created scope for classes and their factories. Classes now varies with dmg dealt, attack speed, attack range, health, and movement speed. Class selection eists via msg.post()
    main.script receives message on which selects factory. Example: "create<class>"

--- Desktop/player/playerClasses/knight/player.go
+++ Desktop/player/playerClasses/knight/player.go
@@ -1,6 +1,6 @@
 components {
-  id: "rangeAttack"
-  component: "/player/rangeAttack.script"
+  id: "player"
+  component: "/player/playerClasses/knight/player.script"
   position {
     x: 0.0
     y: 0.0
@@ -13,61 +13,9 @@ components {
     w: 1.0
   }
 }
-embedded_components {
-  id: "sprite"
-  type: "sprite"
-  data: "tile_set: \"/main/main.atlas\"\n"
-  "default_animation: \"attack_prot\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
-  id: "enemiesCollider"
-  type: "collisionobject"
-  data: "collision_shape: \"\"\n"
-  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
-  "mass: 0.0\n"
-  "friction: 0.1\n"
-  "restitution: 0.5\n"
-  "group: \"attacks\"\n"
-  "mask: \"enemies\"\n"
-  "embedded_collision_shape {\n"
-  "  shapes {\n"
-  "    shape_type: TYPE_BOX\n"
-  "    position {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
-  "    }\n"
-  "    rotation {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
-  "      w: 1.0\n"
-  "    }\n"
-  "    index: 0\n"
-  "    count: 3\n"
-  "  }\n"
-  "  data: 8.0\n"
-  "  data: 8.0\n"
-  "  data: 10.0\n"
-  "}\n"
-  "linear_damping: 0.0\n"
-  "angular_damping: 0.0\n"
-  "locked_rotation: false\n"
-  ""
+components {
+  id: "attack"
+  component: "/player/playerClasses/knight/attack.script"
   position {
     x: 0.0
     y: 0.0
@@ -88,7 +36,7 @@ embedded_components {
   "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"attacks\"\n"
+  "group: \"players\"\n"
   "mask: \"arena\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
@@ -107,13 +55,98 @@ embedded_components {
   "    index: 0\n"
   "    count: 3\n"
   "  }\n"
-  "  data: 8.0\n"
-  "  data: 8.0\n"
+  "  data: 32.0\n"
+  "  data: 32.0\n"
   "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
   "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "enemyCollider"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"players\"\n"
+  "mask: \"enemies\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 32.0\n"
+  "  data: 32.0\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/main.atlas\"\n"
+  "default_animation: \"player_up\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "attackFactory"
+  type: "factory"
+  data: "prototype: \"/player/playerClasses/knight/projectile.go\"\n"
+  "load_dynamically: false\n"
   ""
   position {
     x: 0.0
