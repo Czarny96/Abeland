744a2e477ddb249192f1058efe6bb2032f94b28b
Rafał Podgórski <rafalpodgorski96@gmail.com>
2019-03-31 15:20:52 +0200

    [dev] enemy now follow closest player properly

--- Desktop/main/globals.lua
+++ Desktop/main/globals.lua
@@ -10,9 +10,26 @@ function getPlayersIDs()
 
 	if #returnPlayersIDs() > 0 then
 		for i, player in pairs(playersIDs) do 
-			table.insert(tableOfPlayersIDs, string.sub(tostring(player), 8, #tostring(player)-1))
+			local unhasedID = string.sub(tostring(player), 8, #tostring(player)-1)
+			table.insert(tableOfPlayersIDs, unhasedID)
 		end
 		return tableOfPlayersIDs
+	else
+		return 0
+	end
+end
+
+-- Get all players positions
+function getPlayersPos()
+	local playersIDs = getPlayersIDs()
+	local tableOfPlayersPos = {}
+
+	if playersIDs ~= 0 then
+		for i, player in pairs(playersIDs) do 
+			local playerPos = go.get(playersIDs[i].."#player", "position")
+			table.insert(tableOfPlayersPos, playerPos)
+		end
+		return tableOfPlayersPos
 	else
 		return 0
 	end
