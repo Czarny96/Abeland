8f77f284a6a144db4cfd84c05352bd1ceefb3152
Mateusz Szczepański <mateuszszczepanski00@gmail.com>
2019-04-04 13:42:41 +0200

    First Phase of Optimalisation and Refactoring
    [DEV] adde clientsManager module
    [FIX] return of only active players
    [Refactoring] practicall rewriting of whole code, to work with new module

---
+++ Desktop/server/clientsManager.lua
@@ -0,0 +1,84 @@
+-- Put functions in this file to use them in several other scripts.
+-- To get access to the functions, you need to put:
+-- require "my_directory.my_file"
+-- in any script using the functions.
+local M = {}
+
+local playersTable = {}
+local ALLOWED_MAX_PLAYERS = 4
+local amountOfCurrentPlayers = 0
+
+function M.getAmountOfCurrentPlayers()
+	return amountOfCurrentPlayers
+end
+
+function M.isPlayerActive(playerIP)
+	return playersTable[playerIP][4] == true
+end
+
+function M.activatePlayer(playerIP)
+	playersTable[playerIP][4] = true
+end
+
+function M.desactivatePlayer(playerIP)
+	playersTable[playerIP][4] = false
+end
+
+
+
+function M.isAnyPlayer()
+	return  amountOfCurrentPlayers > 0
+end
+
+function M.addPlayer(playerIP,client,factoryObjectID)
+	if amountOfCurrentPlayers < ALLOWED_MAX_PLAYERS then 
+		-- key: playerIP values: 1 - client; 2 - factoryObjectID; 3 - inactivity time counter; 4 - is player inactive?
+		playersTable[playerIP] = {client,factoryObjectID,0.0,true}
+		amountOfCurrentPlayers = amountOfCurrentPlayers + 1 
+	end
+end
+
+function M.removePlayer(playerIP)
+	playersTable[playerIP] = nil
+	mountOfCurrentPlayers = amountOfCurrentPlayers - 1 
+end
+
+function M.resetPlayerInactivityTimeCounter(playerIP)
+	playersTable[playerIP][3] = 0.0
+end
+
+function M.incrementPlayerInactivityTimeCounterByDT(playerIP,dt)
+	playersTable[playerIP][3] = playersTable[playerIP][3] + dt
+end
+
+function M.getPlayerInactivityTimeCounter(playerIP)
+	return playersTable[playerIP][3]
+end
+
+function M.getPlayerID(playerIP)
+	return playersTable[playerIP][2]
+end
+
+function M.returnActivePlayersIDs()
+	local playerIDs = {}
+	for ip,values in pairs(playersTable) do
+		if M.isPlayerActive(ip) then
+			playerIDs[ip] = M.getPlayerID(ip)
+		end
+	end
+	return playerIDs
+end
+
+function M.returnActiveClients()
+	local clients = {}
+	for ip,values in pairs(playersTable) do
+		if M.isPlayerActive(ip) then
+			clients[ip] = values[1]
+		end
+	end
+	return clients 
+end
+
+
+
+return M
