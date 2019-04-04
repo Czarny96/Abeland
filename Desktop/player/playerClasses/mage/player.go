6fa43f16e6afe4e0562f2116716f2cad9a249f9e
Wiktor <w.grudzina@gmail.com>
2019-04-04 21:09:42 +0200

    [dev] inactive players blinks and is not vulnerable
    untill he moves (possible after 2 seconds of control block)

--- Desktop/player/playerClasses/mage/player.go
+++ Desktop/player/playerClasses/mage/player.go
@@ -143,6 +143,53 @@ embedded_components {
   }
 }
 embedded_components {
+  id: "enemyCollider"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"players\"\n"
+  "mask: \"enemies\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 15.383\n"
+  "  data: 26.982\n"
+  "  data: 11.4\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
   id: "wallsBottomCollider"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
@@ -157,7 +204,7 @@ embedded_components {
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
   "      x: 0.0\n"
-  "      y: -4.0\n"
+  "      y: -9.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -169,8 +216,8 @@ embedded_components {
   "    index: 0\n"
   "    count: 3\n"
   "  }\n"
-  "  data: 10.0\n"
-  "  data: 4.0\n"
+  "  data: 12.0\n"
+  "  data: 1.0\n"
   "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
@@ -204,7 +251,7 @@ embedded_components {
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
   "      x: -11.0\n"
-  "      y: 0.0\n"
+  "      y: -7.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -217,7 +264,7 @@ embedded_components {
   "    count: 3\n"
   "  }\n"
   "  data: 4.0\n"
-  "  data: 5.0\n"
+  "  data: 1.0\n"
   "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
@@ -251,7 +298,7 @@ embedded_components {
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
   "      x: 11.0\n"
-  "      y: 0.0\n"
+  "      y: -7.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -264,7 +311,7 @@ embedded_components {
   "    count: 3\n"
   "  }\n"
   "  data: 4.0\n"
-  "  data: 5.0\n"
+  "  data: 1.0\n"
   "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
@@ -298,7 +345,7 @@ embedded_components {
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
   "      x: 0.0\n"
-  "      y: 4.0\n"
+  "      y: -5.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -310,56 +357,9 @@ embedded_components {
   "    index: 0\n"
   "    count: 3\n"
   "  }\n"
+  "  data: 12.0\n"
+  "  data: 1.0\n"
   "  data: 10.0\n"
-  "  data: 4.0\n"
-  "  data: 10.0\n"
-  "}\n"
-  "linear_damping: 0.0\n"
-  "angular_damping: 0.0\n"
-  "locked_rotation: false\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
-  id: "enemyCollider"
-  type: "collisionobject"
-  data: "collision_shape: \"\"\n"
-  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
-  "mass: 0.0\n"
-  "friction: 0.1\n"
-  "restitution: 0.5\n"
-  "group: \"players\"\n"
-  "mask: \"enemies\"\n"
-  "embedded_collision_shape {\n"
-  "  shapes {\n"
-  "    shape_type: TYPE_BOX\n"
-  "    position {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
-  "    }\n"
-  "    rotation {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
-  "      w: 1.0\n"
-  "    }\n"
-  "    index: 0\n"
-  "    count: 3\n"
-  "  }\n"
-  "  data: 15.383\n"
-  "  data: 26.982\n"
-  "  data: 11.4\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
