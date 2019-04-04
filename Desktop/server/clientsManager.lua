01bdcefbab6434831ed510a95c58fb4e70be4e28
Mateusz Szczepański <mateuszszczepanski00@gmail.com>
2019-04-04 16:47:53 +0200

    Refactoring Finished
    [Refactor] Integrated translation layer with clients manager

--- Desktop/server/clientsManager.lua
+++ Desktop/server/clientsManager.lua
@@ -7,6 +7,7 @@ local M = {}
 local playersTable = {}
 local ALLOWED_MAX_PLAYERS = 4
 local amountOfCurrentPlayers = 0
+local translationLayer = require "server.TranslationLayer"
 
 function M.getAmountOfCurrentPlayers()
 	return amountOfCurrentPlayers
@@ -17,30 +18,17 @@ function M.isPlayerActive(playerIP)
 end
 
 function M.activatePlayer(playerIP)
+	translationLayer.activatePlayer(M.getPlayerID(playerIP))
 	playersTable[playerIP][4] = true
 end
 
 function M.desactivatePlayer(playerIP)
+	translationLayer.desactivatePlayer(M.getPlayerID(playerIP))
 	playersTable[playerIP][4] = false
 end
 
-
-
 function M.isAnyPlayer()
 	return  amountOfCurrentPlayers > 0
-end
-
-function M.addPlayer(playerIP,client,factoryObjectID)
-	if amountOfCurrentPlayers < ALLOWED_MAX_PLAYERS then 
-		-- key: playerIP values: 1 - client; 2 - factoryObjectID; 3 - inactivity time counter; 4 - is player inactive?
-		playersTable[playerIP] = {client,factoryObjectID,0.0,true}
-		amountOfCurrentPlayers = amountOfCurrentPlayers + 1 
-	end
-end
-
-function M.removePlayer(playerIP)
-	playersTable[playerIP] = nil
-	mountOfCurrentPlayers = amountOfCurrentPlayers - 1 
 end
 
 function M.resetPlayerInactivityTimeCounter(playerIP)
@@ -79,6 +67,25 @@ function M.returnActiveClients()
 	return clients 
 end
 
+function M.translateDataToPlayer(data, playerIP)
+	translationLayer.translateFrameToPlayer(data, M.getPlayerID(playerIP))
+end
+
+function M.addPlayer(playerIP,client)
+	if amountOfCurrentPlayers < ALLOWED_MAX_PLAYERS then 
+		-- WARNING: Temporally fixed to call one factory, until mechanism to chose class at the controller side is avaible
+		factoryObjectID = translationLayer.createPlayerObject("go#archerFactory", vmath.vector3(147, 297, 0))
+		-- key: playerIP values: 1 - client; 2 - factoryObjectID; 3 - inactivity time counter; 4 - is player inactive?
+		playersTable[playerIP] = {client,factoryObjectID,0.0,true}
+		amountOfCurrentPlayers = amountOfCurrentPlayers + 1 
+	end
+end
+
+function M.removePlayer(playerIP)
+	playersTable[playerIP] = nil
+	mountOfCurrentPlayers = amountOfCurrentPlayers - 1 
+	translationLayer.RemovePlayerObject(M.getPlayerID(playerIP))
+end
 
 
 return M
