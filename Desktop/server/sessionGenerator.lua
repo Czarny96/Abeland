7af51c63f24d8e5f1c727d77f80c591d11e7dd45
Mateusz Szczepański <mateuszszczepanski00@gmail.com>
2019-04-14 13:14:18 +0200

    [DEV] session destructor skeleton

--- Desktop/server/sessionGenerator.lua
+++ Desktop/server/sessionGenerator.lua
@@ -7,20 +7,22 @@ s_SessionCounter = 0
 
 --- Creates a new session
 function M.create()
-	local session = {}
-
-	--Creating session ID
-	s_SessionCounter = s_SessionCounter + 1
-	session.sessionID = s_SessionCounter
-	print("Creating a new session with ID: " .. session.sessionID)
 	
-	local function localFunctionSkeleton()
+	local function isGameOver()
+		return false
 	end
 
+	local session = {}
+	
 	--- Starts the session 
 	-- @return success
 	-- @return error_message
 	function session.start()
+		
+		--Creating session ID
+		s_SessionCounter = s_SessionCounter + 1
+		session.sessionID = s_SessionCounter
+		print("Creating a new session with ID: " .. session.sessionID)
 		
 		local ok, err = pcall(function()
 			--TODO: check what pcall does
@@ -31,24 +33,25 @@ function M.create()
 
 	-- ends session and releases resources
 	function session.destroy()
-
+		--Add code here to clean up session
+		print("Destroying session with ID: " .. session.sessionID)
+		session = nil
 	end
 
 
-	function server.functionSkeleton()
+	function session.functionSkeleton()
 
 	end
 
 	function session.update()
-		--corutine skeleton
-		coroutine.wrap(function()
-
-		end)
-
-
+		--check if game over
+		if isGameOver() then
+			session.destroy()
+		end
+		return session
 	end
 
-	return session
+	return session.update()
 end
 
 return M
