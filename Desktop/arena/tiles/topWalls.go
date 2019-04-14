5007cc5821a98d38746b46afc1be2d019e90394e
TheSharpClaw <rafalpodgorski96@gmail.com>
2019-04-14 23:45:45 +0200

    [dev] players collide with open gates to prevent leaving from arena

--- Desktop/arena/tiles/topWalls.go
+++ Desktop/arena/tiles/topWalls.go
@@ -106,3 +106,29 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "playerColliderWithOpenGates"
+  type: "collisionobject"
+  data: "collision_shape: \"/arena/topWalls.tilemap\"\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"openGates\"\n"
+  "mask: \"players\"\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
